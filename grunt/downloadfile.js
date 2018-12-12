0e587db499f79de9e1990763a52b75f476642abd
Kai Neuwerth <kai@webchimp.de>
2018-12-12 20:37:36 +0100

    add download of video assets to deployment, update node modules

---
+++ grunt/downloadfile.js
@@ -0,0 +1,14 @@
+module.exports = {
+    files: [
+        {
+            url: 'http://data.neuwerth.me/kai.neuwerth.me/video/shutterstock_v16059430.webm',
+            dest: 'assets/video',
+            name: 'shutterstock_v16059430.webm'
+        },
+        {
+            url: 'http://data.neuwerth.me/kai.neuwerth.me/video/shutterstock_v16059430.mp4',
+            dest: 'assets/video',
+            name: 'shutterstock_v16059430.mp4'
+        }
+    ]
+};
