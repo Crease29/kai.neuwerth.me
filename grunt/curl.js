221141d1faa6caefc4e7414c682ecb3724f36c21
Kai Neuwerth <kai.neuwerth@netshops.de>
2018-12-19 17:36:38 +0100

    Fix code indention

--- grunt/curl.js
+++ grunt/curl.js
@@ -1,7 +1,7 @@
 module.exports = {
-        'assets/css/googlefonts.css': 'https://fonts.googleapis.com/css?family=Quicksand|Ubuntu+Mono',
-        'assets/js/gtag.js': 'https://www.googletagmanager.com/gtag/js?id=UA-115091871-1',
-        'assets/js/cookiebar-latest.min.js': 'https://cdn.jsdelivr.net/npm/cookie-bar/cookiebar-latest.min.js?theme=flying&tracking=1&noGeoIp=1',
-        'assets/video/shutterstock_v16059430.webm': 'http://data.neuwerth.me/kai.neuwerth.me/video/shutterstock_v16059430.webm',
-        'assets/video/shutterstock_v16059430.mp4': 'http://data.neuwerth.me/kai.neuwerth.me/video/shutterstock_v16059430.mp4',
+    'assets/css/googlefonts.css': 'https://fonts.googleapis.com/css?family=Quicksand|Ubuntu+Mono',
+    'assets/js/gtag.js': 'https://www.googletagmanager.com/gtag/js?id=UA-115091871-1',
+    'assets/js/cookiebar-latest.min.js': 'https://cdn.jsdelivr.net/npm/cookie-bar/cookiebar-latest.min.js?theme=flying&tracking=1&noGeoIp=1',
+    'assets/video/shutterstock_v16059430.webm': 'http://data.neuwerth.me/kai.neuwerth.me/video/shutterstock_v16059430.webm',
+    'assets/video/shutterstock_v16059430.mp4': 'http://data.neuwerth.me/kai.neuwerth.me/video/shutterstock_v16059430.mp4',
 };
